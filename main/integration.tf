51fdad9c0ce825f4c363f130950cc2b4f874ffba
chavo1 <chavdar.rakov@gmail.com>
2020-05-29 15:17:51 +0300

    int3

--- main/integration.tf
+++ main/integration.tf
@@ -10,8 +10,3 @@ resource "null_resource" "integration" {
   }
 }
 
-resource "null_resource" "integration1" {
-  provisioner "local-exec" {
-    command = "echo ${random_id.integration.hex}"
-  }
-}
